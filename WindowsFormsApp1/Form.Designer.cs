a3bd9184311885804b232adcc4496f7adfb12a09
Vic <juicebox.south@gmail.com>
2018-06-13 23:35:42 +0100

    Initial Commit of Program

---
+++ WindowsFormsApp1/Form.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Renamer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Renamer));
+            this.lbxFiles = new System.Windows.Forms.ListBox();
+            this.btnAddFile = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnRemoveFile = new System.Windows.Forms.Button();
+            this.tbxFileName = new System.Windows.Forms.TextBox();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.btnClearList = new System.Windows.Forms.Button();
+            this.prgFiles = new System.Windows.Forms.ProgressBar();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.tbxProgress = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbxFiles
+            // 
+            this.lbxFiles.FormattingEnabled = true;
+            this.lbxFiles.Location = new System.Drawing.Point(13, 13);
+            this.lbxFiles.Name = "lbxFiles";
+            this.lbxFiles.Size = new System.Drawing.Size(239, 368);
+            this.lbxFiles.TabIndex = 0;
+            // 
+            // btnAddFile
+            // 
+            this.btnAddFile.Location = new System.Drawing.Point(13, 441);
+            this.btnAddFile.Name = "btnAddFile";
+            this.btnAddFile.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFile.TabIndex = 1;
+            this.btnAddFile.Text = "Add";
+            this.btnAddFile.UseVisualStyleBackColor = true;
+            this.btnAddFile.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(177, 500);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnRemoveFile
+            // 
+            this.btnRemoveFile.Location = new System.Drawing.Point(95, 441);
+            this.btnRemoveFile.Name = "btnRemoveFile";
+            this.btnRemoveFile.Size = new System.Drawing.Size(75, 23);
+            this.btnRemoveFile.TabIndex = 3;
+            this.btnRemoveFile.Text = "Remove";
+            this.btnRemoveFile.UseVisualStyleBackColor = true;
+            this.btnRemoveFile.Click += new System.EventHandler(this.btnRemoveFile_Click);
+            // 
+            // tbxFileName
+            // 
+            this.tbxFileName.Location = new System.Drawing.Point(72, 474);
+            this.tbxFileName.Name = "tbxFileName";
+            this.tbxFileName.Size = new System.Drawing.Size(180, 20);
+            this.tbxFileName.TabIndex = 4;
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(12, 477);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(54, 13);
+            this.lblFileName.TabIndex = 5;
+            this.lblFileName.Text = "File Name";
+            // 
+            // btnClearList
+            // 
+            this.btnClearList.Location = new System.Drawing.Point(177, 441);
+            this.btnClearList.Name = "btnClearList";
+            this.btnClearList.Size = new System.Drawing.Size(75, 23);
+            this.btnClearList.TabIndex = 8;
+            this.btnClearList.Text = "Clear";
+            this.btnClearList.UseVisualStyleBackColor = true;
+            this.btnClearList.Click += new System.EventHandler(this.btnClearList_Click);
+            // 
+            // prgFiles
+            // 
+            this.prgFiles.Location = new System.Drawing.Point(13, 414);
+            this.prgFiles.Name = "prgFiles";
+            this.prgFiles.Size = new System.Drawing.Size(239, 23);
+            this.prgFiles.TabIndex = 9;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.Multiselect = true;
+            // 
+            // tbxProgress
+            // 
+            this.tbxProgress.Location = new System.Drawing.Point(13, 388);
+            this.tbxProgress.Name = "tbxProgress";
+            this.tbxProgress.ReadOnly = true;
+            this.tbxProgress.Size = new System.Drawing.Size(239, 20);
+            this.tbxProgress.TabIndex = 10;
+            // 
+            // Renamer
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 532);
+            this.Controls.Add(this.tbxProgress);
+            this.Controls.Add(this.prgFiles);
+            this.Controls.Add(this.btnClearList);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.tbxFileName);
+            this.Controls.Add(this.btnRemoveFile);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnAddFile);
+            this.Controls.Add(this.lbxFiles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Renamer";
+            this.Text = "Renamer";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Renamer_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Renamer_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxFiles;
+        private System.Windows.Forms.Button btnAddFile;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnRemoveFile;
+        private System.Windows.Forms.TextBox tbxFileName;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Button btnClearList;
+        private System.Windows.Forms.ProgressBar prgFiles;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox tbxProgress;
+    }
+}
+
---
+++ WindowsFormsApp1/Form.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApp1
+{
+    public partial class Renamer : Form
+    {
+
+        List<string> files = new List<string>();
+
+        public Renamer()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                insertionOperation(openFileDialog.FileNames);
+            }
+        }
+
+        private void btnRemoveFile_Click(object sender, EventArgs e)
+        {
+            if (lbxFiles.SelectedIndex >= 0)
+            {
+                files.RemoveAt(lbxFiles.SelectedIndex);
+                lbxFiles.Items.RemoveAt(lbxFiles.SelectedIndex);
+            }
+        }
+
+        private void btnClearList_Click(object sender, EventArgs e)
+        {
+            lbxFiles.Items.Clear();
+            files.Clear();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string renameText = tbxFileName.Text;
+            for(int x = 0; x < files.Count(); x++)
+            {
+                String fileName = files[x].Substring(0, files[x].LastIndexOf("\\"));
+                System.IO.File.Move(files[x], fileName + "\\" + renameText + " " + x);
+                prgFiles.Value = (x+1)/files.Count() * 100;
+                tbxProgress.Text = "changing name of " + files[x].Substring(fileName.LastIndexOf("\\") + 1);
+            }
+            tbxProgress.Text = "Completed";
+            lbxFiles.Items.Clear();
+            files.Clear();
+        }
+
+        private void Renamer_DragDrop(object sender, DragEventArgs e)
+        {
+            //Store Dropped items in array
+            string[] droppedFiles = (string[]) e.Data.GetData(DataFormats.FileDrop);
+            insertionOperation(droppedFiles);
+        }
+
+        private void Renamer_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+
+
+        private void insertionOperation(string[] input)
+        {
+            foreach (string file in input)
+            {
+                bool flagExist = false;
+                foreach (string item in files)
+                {
+                    if (item == file)
+                    {
+                        flagExist = true;
+                    }
+                }
+                if (!flagExist)
+                {
+                    lbxFiles.Items.Add(file.Substring(file.LastIndexOf("\\") + 1));
+                    files.Add(file);
+                }
+            }
+        }
+    }
+}
